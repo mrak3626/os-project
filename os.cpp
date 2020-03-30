ff62daf475fca9dd97a81c041a28d6ae04b68f06
ABHISHEK KUMAR <48253523+mrak3626@users.noreply.github.com>
2020-03-30 21:03:30 +0530

    Update os.cpp

--- os.cpp
+++ os.cpp
@@ -1,3 +1,7 @@
+/*3. Write a multithreaded program in C that outputs prime numbers. This program should
+work as follows: The user will run the program and will enter a number on the command
+line. The program will then create a separate thread that outputs all the prime numbers*/
+less than or equal to the number entered by the user.
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
